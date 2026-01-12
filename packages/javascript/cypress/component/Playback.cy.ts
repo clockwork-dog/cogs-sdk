016b7bd8f674d3b809d2aa5d60e273e6babf3b7a
Guy Balaam <guy@clockwork.dog>
2026-01-12 11:11:37 +0000

    Removing stale comment

--- packages/javascript/cypress/component/Playback.cy.ts
+++ packages/javascript/cypress/component/Playback.cy.ts
@@ -11,13 +11,12 @@ describe('Playback tests', () => {
         fit: 'cover',
         keyframes: [
           [now, { set: { t: 0, rate: 0 } }], // paused at start
-          [now + 2_000, { set: { t: 0, rate: 0 } }], // play in 2 seconds
+          [now + 60_000, { set: { rate: 1 } }], // play in 1 minute
         ],
       },
     });
     cy.mount(manager.element);
 
-    // Video element is meant to start in 1 minute
     cy.get('video').should('have.prop', 'paused', true);
     cy.get('video').should('have.prop', 'currentTime', 0);
   });
@@ -34,9 +33,6 @@ describe('Playback tests', () => {
       },
     });
     cy.mount(manager.element);
-
-    // Allow video to start playing.
-    // Calling pause() during async play() will throw an error
 
     cy.log('Interfere with video element');
     cy.get('video').should('have.prop', 'paused', false);
@@ -64,11 +60,11 @@ describe('Playback tests', () => {
     cy.mount(manager.element);
 
     cy.log('Interfere with video element');
-    cy.get('video').invoke('prop', 'currentTime', 10);
+    cy.get('video').invoke('prop', 'currentTime', 5);
 
     cy.wait(500);
 
     cy.log('Video should have recovered');
-    cy.get('video').invoke('prop', 'currentTime').should('be.lessThan', 5);
+    cy.get('video').invoke('prop', 'currentTime').should('be.lessThan', 2);
   });
 });
