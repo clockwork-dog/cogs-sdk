26b9dc4c758ccd67225421358a0b9c10d17807a0
Guy Balaam <guy@clockwork.dog>
2026-01-22 21:33:52 +0000

    Adding screenshot testing

--- packages/javascript/cypress/support/commands.ts
+++ packages/javascript/cypress/support/commands.ts
@@ -8,30 +8,44 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+import { z } from 'zod';
+type Color = z.infer<typeof rgb>;
+const rgb = z.object({
+  r: z.int().gte(0).lte(255),
+  g: z.int().gte(0).lte(255),
+  b: z.int().gte(0).lte(255),
+});
+
+Cypress.Commands.add('getPixelAt', (x: number, y: number) => {
+  const id = '123';
+  cy.screenshot(id, { overwrite: true, capture: 'viewport' });
+  cy.task('get-pixel-value', { id, x, y }).then(($response) => {
+    return rgb.parse($response);
+  });
+});
+
+// If all r,g,b are within ε of the expected value the color is expected
+// An identical web page will be rendered differently depending on the color profile.
+// This will be different on different machines, and displays
+const RGB_ε = 15;
+Cypress.Commands.add('assertPixelAt', (x: number, y: number, color: Color) => {
+  cy.getPixelAt(x, y).then(($response) => {
+    expect($response.r).to.be.closeTo(color.r, RGB_ε);
+    expect($response.g).to.be.closeTo(color.g, RGB_ε);
+    expect($response.b).to.be.closeTo(color.b, RGB_ε);
+  });
+});
+
+declare global {
+  //   eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      getPixelAt(x: number, y: number): Chainable<Color>;
+      assertPixelAt(x: number, y: number, color: Color): Chainable<void>;
+    }
+  }
+}
+
+// Required to declare global
+export {};
